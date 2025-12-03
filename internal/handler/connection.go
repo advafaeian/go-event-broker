75c3fee187c76f96b6aa0987fcf09397e2e7584c
humahn <isenhall@yahoo.com>
2025-12-03 11:56:43 +0330

    fix reading request message size

--- internal/handler/connection.go
+++ internal/handler/connection.go
@@ -10,33 +10,45 @@ import (
 func HandleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	var buf = make([]byte, 12)
-	_, err := io.ReadFull(conn, buf)
+	var sizeBuf = make([]byte, 4)
+	_, err := io.ReadFull(conn, sizeBuf)
+	if err != nil {
+		log.Printf("Error reading message size from connection: %v", err)
+		return
+	}
+
+	var reqBuf = make([]byte, protocol.BytesToInt32(sizeBuf))
+	_, err = io.ReadFull(conn, reqBuf)
 	if err != nil {
 		log.Printf("Error reading from connection: %v", err)
 		return
 	}
 
 	request := protocol.Request{}
-	if err := protocol.ParseRequest(buf, &request); err != nil {
+	if err := protocol.ParseRequest(reqBuf, &request); err != nil {
 		log.Printf("Error parsing the request: %v", err)
 		return
 	}
 
-	errorCode := int16(0)
+	var response protocol.ApiVersionsResponse
 	if err := request.Validate(); err != nil {
 		pErrCode := err.(*protocol.ProtocolError).Code
-		switch pErrCode {
-		case protocol.ErrUnsupportedVersion.Code:
-			log.Printf("Error validating response: %v", err)
-			errorCode = pErrCode
+		log.Printf("Error validating response: %v", err)
+		response = protocol.ApiVersionsResponse{
+			CorrelationID: request.CorrelationId,
+			ErrorCode:     pErrCode,
 		}
-	}
-
-	response := protocol.Response{
-		MessageSize:   0,
-		CorrelationID: request.CorrelationId,
-		ErrorCode:     errorCode,
+	} else {
+		response = protocol.ApiVersionsResponse{
+			CorrelationID: request.CorrelationId,
+			ErrorCode:     int16(0),
+			ApiKeys: []protocol.ApiKey{{ApiKey: 1, MinVersion: 0, MaxVersion: 11, TagBuffer: []protocol.TaggedField{}},
+				{ApiKey: 18, MinVersion: 0, MaxVersion: 4, TagBuffer: []protocol.TaggedField{}},
+				{ApiKey: 75, MinVersion: 0, MaxVersion: 0, TagBuffer: []protocol.TaggedField{}},
+			},
+			ThrottleMs: 0,
+			TagBuffer:  []protocol.TaggedField{},
+		}
 	}
 
 	_, err = conn.Write(response.Encode())
