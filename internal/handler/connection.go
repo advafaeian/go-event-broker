6e77c390b132fab4d1de743373b717f45a153ec0
humahn <isenhall@yahoo.com>
2025-12-04 10:40:45 +0330

    handle multiple requests in one connection

--- internal/handler/connection.go
+++ internal/handler/connection.go
@@ -9,46 +9,47 @@ import (
 
 func HandleConnection(conn net.Conn) {
 	defer conn.Close()
-
-	var sizeBuf = make([]byte, 4)
-	_, err := io.ReadFull(conn, sizeBuf)
-	if err != nil {
-		log.Printf("Error reading message size from connection: %v", err)
-		return
-	}
-
-	var reqBuf = make([]byte, protocol.BytesToInt32(sizeBuf))
-	_, err = io.ReadFull(conn, reqBuf)
-	if err != nil {
-		log.Printf("Error reading from connection: %v", err)
-		return
-	}
-
-	request := protocol.Request{}
-	if err := protocol.ParseRequest(reqBuf, &request); err != nil {
-		log.Printf("Error parsing the request: %v", err)
-		return
-	}
-
-	response := protocol.ApiVersionsResponse{
-		ResponseHeader: protocol.ResponseHeader{CorrelationID: request.CorrelationId},
-	}
-
-	if err := request.Validate(); err != nil {
-		pErrCode := err.(*protocol.ProtocolError).Code
-		log.Printf("Error validating response: %v", err)
-		response.ErrorCode = pErrCode
-	} else {
-		response.ErrorCode = protocol.NoError
-		response.ApiKeys = []protocol.ApiKey{{ApiKey: 1, MinVersion: 0, MaxVersion: 11, TagBuffer: []protocol.TaggedField{}},
-			{ApiKey: 18, MinVersion: 0, MaxVersion: 4, TagBuffer: []protocol.TaggedField{}},
-			{ApiKey: 75, MinVersion: 0, MaxVersion: 0, TagBuffer: []protocol.TaggedField{}},
+	for {
+		var sizeBuf = make([]byte, 4)
+		_, err := io.ReadFull(conn, sizeBuf)
+		if err != nil {
+			log.Printf("Error reading message size from connection: %v", err)
+			return
 		}
-	}
 
-	_, err = conn.Write(response.Encode())
-	if err != nil {
-		log.Printf("Error writing response: %v", err)
-		return
+		var reqBuf = make([]byte, protocol.BytesToInt32(sizeBuf))
+		_, err = io.ReadFull(conn, reqBuf)
+		if err != nil {
+			log.Printf("Error reading from connection: %v", err)
+			return
+		}
+
+		request := protocol.Request{}
+		if err := protocol.ParseRequest(reqBuf, &request); err != nil {
+			log.Printf("Error parsing the request: %v", err)
+			return
+		}
+
+		response := protocol.ApiVersionsResponse{
+			ResponseHeader: protocol.ResponseHeader{CorrelationID: request.CorrelationId},
+		}
+
+		if err := request.Validate(); err != nil {
+			pErrCode := err.(*protocol.ProtocolError).Code
+			log.Printf("Error validating response: %v", err)
+			response.ErrorCode = pErrCode
+		} else {
+			response.ErrorCode = protocol.NoError
+			response.ApiKeys = []protocol.ApiKey{{ApiKey: 1, MinVersion: 0, MaxVersion: 11, TagBuffer: []protocol.TaggedField{}},
+				{ApiKey: 18, MinVersion: 0, MaxVersion: 4, TagBuffer: []protocol.TaggedField{}},
+				{ApiKey: 75, MinVersion: 0, MaxVersion: 0, TagBuffer: []protocol.TaggedField{}},
+			}
+		}
+
+		_, err = conn.Write(response.Encode())
+		if err != nil {
+			log.Printf("Error writing response: %v", err)
+			return
+		}
 	}
 }
