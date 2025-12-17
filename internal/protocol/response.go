cd944abb84e70d4d5e6561deaf7f8651e4dd6c78
humahn <isenhall@yahoo.com>
2025-12-17 16:27:50 +0330

    add tag buffer to the response header

--- internal/protocol/response.go
+++ internal/protocol/response.go
@@ -2,8 +2,10 @@ package protocol
 
 type ResponseHeader struct {
 	CorrelationID int32
+	TagBuffer     TagBuffer
 }
 
 func (r *ResponseHeader) Encode(w *Writer) {
 	w.Int32(r.CorrelationID)
+	w.TagBuffer(r.TagBuffer)
 }
