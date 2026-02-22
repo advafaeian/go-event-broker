58dcb4bbc91d2c51da24cf4df0830bd7767391aa
humahn <isenhall@yahoo.com>
2026-02-22 17:22:39 +0330

    feat: improve fetch response handling

--- internal/protocol/fetch.go
+++ internal/protocol/fetch.go
@@ -1,11 +1,19 @@
 package protocol
 
 type FetchResponse struct {
-	Header    ResponseHeader
-	ErrorCode int16
+	Header     ResponseHeader
+	ThrottleMs int32
+	ErrorCode  int16
+	SessionID  int32
+	Responses  []Topic
+	TagBuffer  TagBuffer
 }
 
 func (r *FetchResponse) Encode(w *Writer) {
 	r.Header.Encode(w, 4)
+	w.Int32(r.ThrottleMs)
 	w.Int16(r.ErrorCode)
+	w.Int32(r.SessionID)
+	w.CompactArrayTopics(r.Responses)
+	w.TagBuffer(r.TagBuffer)
 }
