4b21bee75daa7c67ba90f035591b1acae7129703
humahn <isenhall@yahoo.com>
2026-02-28 16:14:11 +0330

    feat: validate produce request topic and partition

--- internal/handler/produce.go
+++ internal/handler/produce.go
@@ -11,21 +11,65 @@ func HandleProduce(w *protocol.Writer, r *protocol.Reader, metadataLoader *metad
 		return err
 	}
 
-	response := protocol.ProduceResponse{
-		Topics: []protocol.ProduceResponseTopic{
-			{
-				TopicName: request.Topics[0].TopicName,
-				Partitions: []protocol.ProduceResponsePartition{
-					{
-						PartitionID:    request.Topics[0].Partitions[0].PartitionIndex,
-						ErrorCode:      protocol.UnknownTopicOrPartition,
-						BaseOffset:     -1,
-						LogAppendTime:  -1,
-						LogStartOffset: -1,
+	reqTopicName := request.Topics[0].TopicName
+
+	var errorCode int16
+
+	topic, err := metadataLoader.Get(reqTopicName)
+	// validate topic
+	var response protocol.ProduceResponse
+
+	if err != nil {
+		errorCode = protocol.UnknownTopicOrPartition
+	}
+
+	// validating paritions
+	for _, p := range topic.Partitions {
+		reqPartitionIndex := request.Topics[0].Partitions[0].PartitionIndex
+		reqTopicID := topic.TopicID
+
+		_, err := metadata.LoadPartition(topic.TopicName, reqPartitionIndex)
+		if err != nil || reqPartitionIndex != p.PartitionIndex || reqTopicID != p.TopicID {
+			errorCode = protocol.UnknownTopicOrPartition
+			continue
+		}
+	}
+
+	if errCode != 0 {
+		response = protocol.ProduceResponse{
+			Topics: []protocol.ProduceResponseTopic{
+				{
+					TopicName: reqTopicName,
+					Partitions: []protocol.ProduceResponsePartition{
+						{
+							PartitionID:    request.Topics[0].Partitions[0].PartitionIndex,
+							ErrorCode:      errorCode,
+							BaseOffset:     -1,
+							LogAppendTime:  -1,
+							LogStartOffset: -1,
+						},
 					},
 				},
 			},
-		},
+		}
+	} else {
+		response = protocol.ProduceResponse{
+			ThrottleMs: 0,
+			Topics: []protocol.ProduceResponseTopic{
+				{
+					TopicName: topic.TopicName,
+					Partitions: []protocol.ProduceResponsePartition{
+						{
+							ErrorCode:      errorCode,
+							PartitionID:    request.Topics[0].Partitions[0].PartitionIndex,
+							BaseOffset:     0,
+							LogAppendTime:  -1,
+							LogStartOffset: 0,
+						},
+					},
+				},
+			},
+		}
 	}
 
 	response.Encode(w)
